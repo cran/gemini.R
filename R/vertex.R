29af0f477cb436364919b2eb62c4b0ff4e1a4c22
Jinhwan Kim <hwanistic@gmail.com>
2025-03-12 12:10:02 +0000

    version 0.9.2

--- R/vertex.R
+++ R/vertex.R
@@ -4,14 +4,17 @@
 #' @param jsonkey A path to JSON file containing the service account key from Vertex AI.
 #' @param model_id The ID of the Gemini model. This will be prepended with "gemini-".
 #' @param expTime The expiration time of the access token in seconds (default is 3600 seconds, or 1 hour).
+#' @param region The Google Cloud region where your Vertex AI resources are located (default is "us-central1").
+#'        See https://cloud.google.com/vertex-ai/docs/general/locations for available regions.
 #'
 #' @examples
 #' \dontrun{
 #' library(gemini.R)
 #' tokens <- token.vertex(jsonkey = "YOURAPIKEY.json", model_id = "1.5-flash")
-#'
+#' 
+#' # Specify a different region
+#' tokens <- token.vertex(jsonkey = "YOURAPIKEY.json", model_id = "1.5-flash", region = "europe-west4")
 #' }
-#'
 #'
 #' @return A list containing:
 #' \item{key}{The generated access token.}
@@ -21,16 +24,18 @@
 #' @importFrom httr2 jwt_claim jwt_encode_sig request req_body_form req_perform resp_body_json
 #'
 #' @export
-token.vertex <- function(jsonkey = NULL, model_id = NULL, expTime = 3600){
+token.vertex <- function(jsonkey = NULL, model_id = NULL, expTime = 3600, region = "us-central1") {
   account <- fromJSON(jsonkey)
   project_id <- account$project_id
 
-  model_id <- paste0('gemini-', model_id, ':generateContent')
+  model_id <- paste0("gemini-", model_id, ":generateContent")
 
   endpoint_url <- paste0(
-    "https://us-central1-aiplatform.googleapis.com/v1/projects/",
+    "https://",
+    region,
+    "-aiplatform.googleapis.com/v1/projects/",
     project_id,
-    "/locations/us-central1/publishers/google/models/",
+    "/locations/", region, "/publishers/google/models/",
     model_id
   )
 
@@ -39,8 +44,8 @@ token.vertex <- function(jsonkey = NULL, model_id = NULL, expTime = 3600){
   iat <- as.numeric(Sys.time())
 
   jwt_claims <- jwt_claim(
-    iss = account$client_email,  # 서비스 계정 이메일
-    scope = "https://www.googleapis.com/auth/cloud-platform",  # 권한 범위
+    iss = account$client_email, # 서비스 계정 이메일
+    scope = "https://www.googleapis.com/auth/cloud-platform", # 권한 범위
     aud = token_url,
     iat = iat,
     exp = iat + expTime
